efbe6891e15728ef24ee6a86b75a0569f1f9d44d
edu8169 <62029234+edu8169@users.noreply.github.com>
2022-03-12 17:14:45 -0600

    Added branch user-routes

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -5,6 +5,18 @@ class User extends Model {
     constructor() {
         super('users');
     }
+    getbyEmail(email) {
+        return new Promise((resolve, reject) => {
+            Model.collection.findOne({ email: email }, (err, result) => {
+                if(err) {
+                    reject(err);
+                }
+                if(result){
+                    resolve(result);
+                }
+            })
+        })
+    }
 
 }
 
