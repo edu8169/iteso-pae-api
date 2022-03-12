24d2a93e7e3aeecc42711f784fd878a2748a54dc
edu8169 <62029234+edu8169@users.noreply.github.com>
2022-03-12 17:13:42 -0600

    Added create and prototipe Validate

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -16,6 +16,31 @@ const UsersController = {
                 res.sendStatus(404);
             }
         });
+    },
+    create: (req, res) => {
+        const user = new User();
+        const body = req.body;
+        user.create(body).then(result =>{
+            //console.log(result);
+            //body.id = result.insertedId
+            res.json(result);
+        })
+        .catch(err => {
+            res.sendStatus(409);
+        })
+    },
+    //verificar que sea solo un mail registrado dentor de create,checar que la contraseña ingresada seala misma que la que esta guardada
+    validate: (req, res,next) => {
+        const user = new User();
+        const body = req.body;
+        user.getbyEmail(body.email).then(result => {
+            if(result){
+                next();
+            } else {
+                res.sendStatus(409)
+            }
+        })
+        
     }
     
 
